0417636125d02a54f84e09796f18628aa41197ca
gwonyeong <qwe980121@naver.com>
2022-11-05 23:49:39 +0900

    Add: 기본적인 숫자야구 기능 완성

--- numberBaseball/app.js
+++ numberBaseball/app.js
@@ -18,14 +18,77 @@ app.engine("html", require("ejs").renderFile);
 const port = 5000;
 
 app.get("/", (req, res, next) => {
-    console.log(req.query);
-
     res.render("index.html");
 });
-app.get("/num", (req, res, next) => {
-    console.log(req.session);
-    const num = req.session.num;
-    return res.send(req.session);
+
+app.get("/start", (req, res, next) => {
+    const { nickname } = req.query;
+
+    req.session.nickname = nickname;
+    let randomNumber = "";
+    for (let i = 0; i < 4; i++) {
+        temp = parseInt(Math.random() * 9);
+        if (randomNumber.indexOf(temp) != -1) {
+            i--;
+            console.log("겹침");
+        } else {
+            randomNumber += String(temp);
+        }
+    }
+    req.session.randomNumber = randomNumber;
+
+    res.render("baseball.html", {
+        nickname: nickname,
+        randomNumber: "4자리 숫자를 입력해주세요!",
+        answer: false,
+    });
+});
+
+app.get("/answer", (req, res, next) => {
+    const { submitAnswer } = req.query;
+    const { randomNumber } = req.session;
+    try {
+        for (let str of submitAnswer) {
+            if (isNaN(parseInt(str))) {
+                return res.render("baseball.html", {
+                    nickname: req.session.nickname,
+                    randomNumber: "정확한 값을 입력하세요.",
+                    answer: false,
+                });
+            }
+        }
+        let strike = 0,
+            ball = 0;
+        for (let i = 0; i < submitAnswer.length; i++) {
+            const includedNum = randomNumber.indexOf(submitAnswer[i]);
+            if (includedNum != -1) {
+                if (includedNum == i) {
+                    strike++;
+                } else {
+                    ball++;
+                }
+            }
+        }
+        if (strike == 4) {
+            return res.render("baseball.html", {
+                nickname: req.session.nickname,
+                randomNumber: ``,
+                answer: true,
+            });
+        }
+        return res.render("baseball.html", {
+            nickname: req.session.nickname,
+            randomNumber: `strike : ${strike}, ball : ${ball}`,
+            answer: false,
+        });
+    } catch (err) {
+        console.log(err);
+        return res.render("baseball.html", {
+            nickname: req.session.nickname,
+            randomNumber: "정확한 값을 입력하세요.",
+            answer: false,
+        });
+    }
 });
 
 app.listen(port, () => {
